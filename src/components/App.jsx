6d3cdb63b5fab83606420a6e54ecd497be7e652c
Jaan Lambert <jaan.lambert@gmail.com>
2017-05-05 21:24:24 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './App.css'
+
+const App = () => (
+  <div className={styles.app}>
+    <h2>Hello, you whats up?</h2>
+  </div>
+)
+
+export default App
