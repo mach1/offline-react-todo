90d5c7017f4dc71eee570f802f7f47ca6d1ea085
Jaan Lambert <jaan.lambert@gmail.com>
2017-05-05 22:11:54 +0300

    Css loader config

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,16 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader?modules' ]
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]'
+            }
+          }
+        ]
       }
     ]
   },
